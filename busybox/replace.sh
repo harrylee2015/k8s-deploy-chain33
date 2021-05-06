30254875290723b534dd5191e0b28f6f30f44340
harrylee <18761806026@163.com>
2021-05-06 17:32:08 +0800

    修改脚本

--- busybox/replace.sh
+++ busybox/replace.sh
@@ -3,18 +3,28 @@
 # describe: 为支持在k8s中部署chain33而写的init container脚本
 # 环境变量导入
 sleep 10
-resource env
+source ./env
+source ./hosts
 local_hostname=$(hostname)
 local_domain=$(hostname -f)
 hostnames=`cat hosts`
+rm -rf ips
 for i in $hostnames
 do
 domain=$(echo ${local_domain}|sed -e "s/${local_hostname}./${i}./g")
-IP=`ping $i -c 1 |awk 'NR==2 {print $5}' |awk -F ':' '{print $1}' |sed -nr "s#\(##gp"|sed -nr "s#\)##gp"`
+IP=`ping $i -c 1 |awk 'NR==2 {print $4}' |awk -F ':' '{print $1}'`
     echo $i=$IP >> ips
 done
 #导入解析主机信息
-source ips
+source ./ips
 #替换实际变量
-eval "cat<<EOF $(<base.toml) EOF" >chain33.toml
+eval "cat <<EOF
+$(<base.toml)
+EOF
+" >tmp.toml
+if [ "$?" -ne 0 ]; then
+  echo "replace chain33.toml failed!"
+  exit 1
+fi
+mv tmp.toml chain33.toml
 echo "replace chain33.toml sucessfully!"
