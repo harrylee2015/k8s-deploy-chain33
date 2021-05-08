8ddc3a694d8f31d6351f00e7497480522314bd9c
harrylee <18761806026@163.com>
2021-05-08 12:19:56 +0800

    修改chain33镜像构建文件

---
+++ chain33/resource/start.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# author： harry
+# describe: 为支持在k8s中部署chain33而写的在pod中动态修改配置文件脚本
+# 环境变量导入  env其他参数配置信息,hosts主机名称列表
+sleep 10
+source ./env
+local_hostname=$(hostname)
+local_domain=$(hostname -f)
+hostnames=`cat hosts`
+rm -rf ips
+for i in $hostnames
+do
+  domain=$(echo ${local_domain}|sed -e "s/${local_hostname}./${i}./g")
+  for j in `seq 1 100`
+  do
+    echo "try ping times: $j"
+    IP=$(ping $domain -c 1 |awk 'NR==2 {print $5}' |awk -F ':' '{print $1}' |sed -nr "s#\(##gp"|sed -nr "s#\)##gp")
+    if [[ -n "$IP" ]]; then
+      echo "$i=$IP" >> ips
+      break
+    fi
+    sleep 1
+  done
+done
+#导入解析主机信息
+source ./ips
+#替换实际变量
+eval "cat <<EOF
+$(cat base.toml)
+EOF
+" >chain33.toml
+if [ "$?" -ne 0 ]; then
+  echo "replace chain33.toml failed!"
+  exit 1
+fi
+sleep 5
+echo "replace chain33.toml sucessfully!"
+# 启动命令
+./chain33 -f chain33.toml
