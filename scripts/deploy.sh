6641698dfe3e2cd984deff50e50b0af2eecde186
harrylee <18761806026@163.com>
2021-05-20 13:35:15 +0800

    update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+RED='\033[1;31m'
+GRE='\033[1;32m'
+NOC='\033[1;0m'
 #集群名称
 CLUSTER_NAME=$1
 #副本数(节点总数）
@@ -30,43 +33,50 @@ do
     break
   fi
   if [[ 0 -eq $i ]]; then
-    SEEDS=\"\${CLUSTER_NAME}_$i:13802\"
-    VALIDATORNODES=\"\${CLUSTER_NAME}_$i:46656\"
+    SEEDS=\"\${${CLUSTER_NAME}_$i}:13802\"
+    VALIDATORNODES=\"\${${CLUSTER_NAME}_$i}:46656\"
   else
-    SEEDS=${SEEDS},\"\${CLUSTER_NAME}_$i:13802\"
-    VALIDATORNODES=${VALIDATORNODES},\"\${CLUSTER_NAME}_$i:46656\"
+    SEEDS=${SEEDS},\"\${${CLUSTER_NAME}_$i}:13802\"
+    VALIDATORNODES=${VALIDATORNODES},\"\${${CLUSTER_NAME}_$i}:46656\"
   fi
 done
-
+echo -e "${GRE} $SEEDS ${NOC}"
+echo -e "${GRE} $VALIDATORNODES ${NOC}"
 mkdir -p ${DEPLOY_DIR}/${CLUSTER_NAME}
 #从镜像中copy chain33-cli可执行文件，生成私钥
 IMAGE=chain33:1.0.2
-docker pull ${IMAGE}
-docker run --rm --name chain33 chain33:v1.0.2 /bin/bash -c "sleep 30"
-docker cp  chain33:/app/chain33/*.yaml ${DEPLOY_DIR}
+if [  ! -z `docker images -q ${IMAGE}` ]; then
+  docker pull ${IMAGE}
+fi
+docker run -d --rm --name chain33 chain33:v1.0.2 /bin/bash -c "sleep 30"
+docker cp  chain33:/app/chain33/chain33-config.yaml ${DEPLOY_DIR}
+docker cp  chain33:/app/chain33/chain33-service.yaml ${DEPLOY_DIR}
+docker cp  chain33:/app/chain33/chain33-statefulSet.yaml ${DEPLOY_DIR}
+docker cp  chain33:/app/chain33/chain33-storageClass.yaml ${DEPLOY_DIR}
 docker cp  chain33:/app/chain33/chain33-cli ${DEPLOY_DIR}
 
 cd ${DEPLOY_DIR}
 #生成节点私钥
 ./chain33-cli valnode init_keyfile -t bls -n ${REPLICAS}
 #genesis.json
-GENESIS=$(cat genesis.json)
+GENESIS=$(cat genesis_file.json)
 GENESIS="
   genesis.json: |
     ${GENESIS}"
+echo -e "${GRE} $GENESIS ${NOC}"
 #priv_validator
-PRIV_VALIDATOR=""
+PRIV_VADATOR=""
 for i in `seq 0 $REPLICAS`
 do
   if [[ $i -eq $REPLICAS ]]; then
     break
   fi
-  PRIV=$(cat priv_validator_$i)
-  PRIV_VALIDATOR=${PRIV_VALIDATOR}"
-  priv_validator_0.json: |
+  PRIV=$(cat priv_validator_$i.json)
+  PRIV_VADATOR=${PRIV_VADATOR}"
+  priv_validator_$i.json: |
     $PRIV"
 done
-
+echo -e "${GRE} $PRIV_VADATOR ${NOC}"
 #替换实际变量
 eval "cat <<EOF
 $(cat chain33-config.yaml)
@@ -104,7 +114,7 @@ if [ "$?" -ne 0 ]; then
   exit 1
 fi
 
-#开始部署
+##开始部署
 cd ${CLUSTER_NAME}
 ./kubectl apply -f chain33-storageClass.yaml
 sleep 1
